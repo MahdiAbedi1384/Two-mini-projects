98036599e387054f7a267282cb5da831dd60a5fa
Mahdi Abedi <mahdiabedi1384824@gmail.com>
2025-05-26 18:36:04 +0330

    find number file changed

--- findNumber.py
+++ findNumber.py
@@ -3,31 +3,29 @@ from time import time
 
 t1 = time()
 
-# Function to perform binary search
-def binary_search(arr, left, right, target):
-    while left <= right:
-        mid = left + (right - left) // 2
-
-        if arr[mid] == target:
-            return True
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return False
-
 # Function to check whether any pair exists
 # whose sum is equal to the given target value
+
+
 def twoSum(arr, target):
-    arr.sort()
+
+    # Create a set to store the elements
+    s = set()
 
     # Iterate through each element in the array
-    for i in range(len(arr)):
-        complementation = target - arr[i]
+    for num in arr:
 
-        # Use binary search to find the complementation
-        if binary_search(arr, i + 1, len(arr) - 1, complementation):
+        # Calculate the complement that added to
+        # num, equals the target
+        complement = target - num
+
+        # Check if the complement exists in the set
+        if complement in s:
             return True
+
+        # Add the current element to the set
+        s.add(num)
+
     # If no pair is found
     return False
 
@@ -35,6 +33,7 @@ def twoSum(arr, target):
 # create a random array(list) for test functions
 nums = [randint(0, 1000000) for _ in range(1000000)]
 
+# call the function and print result
 print(twoSum(nums, 701))
 
 # measure time
